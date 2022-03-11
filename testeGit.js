6567753a76d27fa98202acf57cda3fc7eec80577
Max Cleiton <maxcleiton@live.com>
2022-03-10 23:17:18 -0300

    main

--- testeGit.js
+++ testeGit.js
@@ -1,1 +1,8 @@
 console.log('teste!!')
+
+testando denovo //vamos commitar
+
+//caso dê erro no botao azul:
+https://github.com/alexandre-blueedtech/C013-M01-LAP/blob/main/aula04-codelab/testeGit.js
+https://git-scm.com/book/pt-br/v2/Come%C3%A7ando-Configura%C3%A7%C3%A3o-Inicial-do-Git
+Educacional Blue EdTech23:12
