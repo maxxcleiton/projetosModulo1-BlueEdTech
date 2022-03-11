a2e0cd51ce7422a34167797119feec28f63895e1
Max Cleiton <maxcleiton@live.com>
2022-03-10 23:32:31 -0300

    Versão 2

--- projetoHeroi.js
+++ projetoHeroi.js
@@ -1,18 +1,20 @@
 var prompt = require('prompt-sync')();
+
 console.clear();
 
-let historia = 'O guerreiro intergalático chegou de sua aventura, onde ele teve de devolver uma criança à sua raça originária, além de conseguir uma armadura do melhor material disponível na galáxia, e também uma lança! com o material encontrado.';
+let historia = 'O guerreiro intergalático chegou de sua aventura, onde ele teve de devolver uma criança à sua raça originária, além de conseguir uma armadura do melhor material disponível na galáxia, e também uma lança com o material encontrado.';
 
+console.log();
 console.log(historia);
 console.log();
 console.log('Responda às perguntas com s ou n (sim ou não):');
 console.log();
 
 let pergunta1=prompt('Devolveu a criança a quem o pertencia? ');
-let pergunta2=prompt('Teve resistência por parte da criança em deixar você? ');
-let pergunta3=prompt('Conseguiu obter o material mais poderoso da galáxia? ')
-let pergunta4=prompt('Com este material, produziu com sucesso a sua armadura? ')
-let pergunta5=prompt('A lança, por ser um material concentrado, perfuraria sua própria armadura? ')
+let pergunta2=prompt('Reparou sua nave, que estava precisando de uns belos reparos? ');
+let pergunta3=prompt('Conseguiu obter o material mais poderoso da galáxia? ');
+let pergunta4=prompt('Com este material, produziu com sucesso a sua armadura? ');
+let pergunta5=prompt('A lança, por ser um material concentrado, perfuraria sua própria armadura? ');
 
 
 // console.log(pergunta1);
@@ -22,30 +24,30 @@ let pergunta5=prompt('A lança, por ser um material concentrado, perfuraria sua 
 // console.log(pergunta5);
 
 let nivel = 0;
-if (pergunta1 = "s") {
-    nivel=nivel+1
+if (pergunta1 == 's') {
+    nivel++
 } 
-if (pergunta2 = 's') {
-    nivel=nivel+1
+if (pergunta2 == 's') {
+    nivel++
 }
-if (pergunta3 = 's') {
-    nivel=nivel+1
+if (pergunta3 == 's') {
+    nivel++
 }
-if (pergunta4 = 's') {
-    nivel=nivel+1
+if (pergunta4 == 's') {
+    nivel++
 }
-if (pergunta5 = 's') {
-    nivel=nivel+1
+if (pergunta5 == 's') {
+    nivel++
 }
 
 if (nivel === 0) {
-    console.log("Desculpa guerreiro mas você não conseguiu nada? deixa que eu passo essa missão para alguém mais apto. ");
+    console.log("Desculpa guerreiro mas você não conseguiu nada? deixa que eu passo essa missão para alguém mais preparado. ");
 } else if (nivel == 1 || nivel == 2) {
     console.log("Olha só, você não concluiu a missão como deveria. Mas pelo menos conseguiu algo feito, então não é um completo desastre.")
 } else if (nivel == 3) {
     console.log("Oha guerreiro, às vezes, o que nos faltava era só mais uma pitadinha a mais de resiliência");
 } else if (nivel == 4) {
-    console.log("Foi uma jornada árdua! então é de se entender que não a fez com \"pura maestria\". Título esse que só pertence aos melhores dos melhores... bom, lâmina quanto mais afia, mais afiada fica.");
+    console.log("Foi uma jornada árdua! então é de se entender não a ter feito com \"pura maestria\". Título esse que só pertence aos melhores dos melhores... bom, lâmina quanto mais afia, mais afiada fica.");
 } else if (nivel ==5 ) {
     console.log("É uma satisfação enorme presenciar em minha ínfima vida... tanta glória, persistência, garra, e valorização dos seus valores como cidadão deste universo. Você não apenas foi e voltou, mas você mudou o rumo do universo, mesmo que não saiba ainda...");
 }
