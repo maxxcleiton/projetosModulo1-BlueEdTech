ac35d7b808c1a7f61088fd7372d74fe4829f3c96
Max Cleiton <maxcleiton@live.com>
2022-03-15 20:49:30 -0300

    Início - variáveis e escolhas

---
+++ projeto_02.js
@@ -0,0 +1,33 @@
+var prompt = require('prompt-sync')();
+
+console.clear();
+
+//Math.floor(Math.random())
+
+//Início
+// Para hoje 15/3
+// O projeto é extenso, e temos muito o que fazer! Mas vamos começar por partes. Para hoje seus objetivos são:
+// Criar uma lista com os elementos a serem escolhidos;
+// Criar as variáveis que receberão as escolhas do usuário e do computador (prompt para o usuário, e aleatório para o computador);
+// Fazer a validação da entrada do usuário (o programa só deve aceitar o que foi definido por você como entradas válidas);
+// Exibir o elemento da lista correspondente à escolha de cada um;
+// Comparar os elementos e exibir quem foi o vencedor dessa rodada.
+
+
+// Criar uma lista com os elementos a serem escolhidos;
+const lista = ['pedra', 'papel', 'tesoura'];
+
+// Criar as variáveis que receberão as escolhas do usuário e do computador (prompt para o usuário, e aleatório para o computador);
+let rodadas = 0;
+let escolha = 
+let escolhaLow = 
+rodadas = +prompt('Escolha quantas rodadas iremos fazer: ');
+
+escolha = prompt('Pedra, papel, ou tesoura?')
+escolhaLow = escolha.toLowerCase();
+
+while (rodadas) {
+
+}
+
+if (rodadas = )
