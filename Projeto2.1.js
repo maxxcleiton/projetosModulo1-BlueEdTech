3f771f0537ad8f73a10df126e317a6428de661d4
Max Cleiton <maxcleiton@live.com>
2022-03-17 20:52:25 -0300

    Adicionados os ifs e contador de vitória

--- Projeto2.1.js
+++ Projeto2.1.js
@@ -4,6 +4,14 @@ console.clear();
 let l = ['pedra', 'papel', 'tesoura'] // Lista
 let u = []; // Escolha do usuário
 let c = []; // Escolha do computador
+let r = 0; // Rodadas (quantidade)
+let uw = 0; // Contador de vitórias do usuário
+let cw = 0; // Contador de vitórias do computador
+
+r = +prompt('Quantas rodadas iremos fazer? ');
+
+for (let x = 0; x < r; x++) { // Criei um contador que gira até ficar menor que r.
+// Posso usar while (r--)
 
 u = prompt('Qual elemento que você escolhe? ');
 console.log(u)
@@ -11,3 +19,31 @@ console.log(u)
 c = Math.floor(Math.random()*3);
 console.log(l[c])
 
+// if do usuário
+if (u == 'tesoura' && l[c] == 'papel') {
+    console.log('Vitória para você! ');
+    uw++
+} else if (u == 'pedra' && l[c] == 'tesoura') {
+    console.log('Vitória para você! ');
+    uw++
+} else if (u == 'papel' && l[c] == 'pedra') {
+    console.log('Vitória para você! ');
+    uw++
+}
+
+// if do computador
+if (l[c] == 'tesoura' && u == 'papel') {
+    console.log('Vitória para o computador! ')
+    cw++
+} else if (l[c] == 'pedra' && u == 'tesoura') {
+    console.log('Vitória para o computador! ')
+    cw++
+} else if (l[c] == 'papel' && u == 'pedra') {
+    console.log('Vitória para o computador! ')
+    cw++
+}
+
+}
+
+console.log(`Total pontos usuário: ${uw}`);
+console.log(`Total pontos computador: ${cw}`)
