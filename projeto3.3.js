fb0bb45d85acc38e66d3bbf342fbf45522f7af87
maxxcleiton~ <maxcleiton@live.com>
2022-04-04 21:20:41 -0300

    Limpeza de código

--- projeto3.3.js
+++ projeto3.3.js
@@ -27,53 +27,29 @@ let nn = array[1] - numero // array desejada de alteracao
 array.splice(1,1,nn) // 0 indice/1 n. itens excluidos/nn variavel operacao
 return array[1] } 
 // encerra alteraTempo
-}
+};
 
 let adicionarHora = {
     maisHora: function(hora){
     alteraTempo.somarArrayHora(tempo,hora)
     }, 
-}
+};
 
 let personagem = {
     nome: ``,
     fome: 15,
     vida: 10,
     segurança: 5
-}
+};
 
-let tempo = [1, 7] // dia e hora
+let tempo = [1, 7] // Dia e Hora
+let jogarNovamente;
 let playagain;
 
-// let whiles = {
-//     morreu: function(){
-//         while (personagem.vida < 0){ 
-//             console.log('voce morreu de vida (< 0)!');   
-//             meusIfs.jogarNovamente() 
-//         } // enquanto a vida for menor que 10, repete (se morrer ele breaka e repete)
-//         while (personagem.fome > 20){ console.log('voce morreu de fome! (> 20)');  meusIfs.jogarNovamente() } // enquanto a vida for maior que 20, repete
-//         while (personagem.segurança < 0){ console.log('a segurança da sua casa foi embora e os zumbis invadiram! (< 0)'); meusIfs.jogarNovamente() } // enquanto a segurança for menor que 0
-        
-//     }
-// }
-
-// let meusIfs = {
-//     jogarNovamente: function(){
-//         if (personagem.vida < 0 || personagem.fome > 20 || personagem.segurança < 0 ){
-//         console.log('deseja jogar novamente? 1. sim 2. nao');
-//         playagain = +prompt();
-//         }
-//         if (playagain == 2) {rodarPrograma = false;}
-//         while (playagain == 1) {playagain = 0; tempo[0] = 1 };
-//         tempo[0] = 1; tempo[1] = 7; personagem.vida = 10; personagem.fome = 15; personagem.segurança = 5;
-    
-//     },
-// }
-while (tempo[0] == 1){ // enquanto o dia for 1
-
+while (tempo[0] == 1){
         console.log(`Dia ${tempo[0]} Hora ${tempo[1]}:00`);
         console.log(`Vida: ${personagem.vida} | Segurança: ${personagem.segurança} | Fome: ${personagem.fome}`);
-        console.log('O tempo vai andar de acordo com suas escolhas, não deixe o personagem morrer\nEstatísticas de morte: vida (< 0) | segurança (< 0) | fome (> 20)');
+        console.log('O tempo vai andar de acordo com suas escolhas, não deixe o personagem morrer!\nEstatísticas de morte: vida (< 0) | segurança (< 0) | fome (> 20)');
 
         // como fazer perguntas que impactem na hora ::::
         // let escolha = +prompt('avance 1h digitando 1\navance 2h digitando 2')
@@ -88,70 +64,56 @@ while (tempo[0] == 1){ // enquanto o dia for 1
         // } // testar isso mais pra frente hein
 
 
-        tempo[1] = +prompt('digite uma hora: ')
-        personagem.vida = +prompt('bote um valor de vida: ') //alterar pra variaveis que modificam a vida
-        personagem.fome = +prompt('bote um valor de fome: ')
-        personagem.segurança = +prompt(`bote um valor de segurança (atual: ${personagem.segurança}): `)
+        tempo[1] = +prompt('digite uma hora: ');
+        personagem.vida = +prompt('bote um valor de vida: ');
+        personagem.fome = +prompt('bote um valor de fome: ');
+        personagem.segurança = +prompt(`bote um valor de segurança (atual: ${personagem.segurança}): `);
 
 
         if (tempo[1] > 23){
             tempo[0] = 2
             tempo[1] = 0
-        }
+        };
 
 
-
-        // Variaveis de encerramento -- funcionando!
+        // Variáveis de encerramento
 
         if (personagem.vida < 0){
-            console.log('voce morreu de vida (< 0)!');
-            console.log(`voce deseja jogar nuevamiente? 1 pra sim e 2 pra nao`);
-            let joganuevamiente = +prompt();
-            if (joganuevamiente == 1){
+            console.log(`Game over! Motivo: sem vida! (< 0)`);
+            console.log(`Você deseja jogar novamente? 1. Sim 2. Não`);
+            jogarNovamente = +prompt();
+            if (jogarNovamente == 1){
                 tempo[0] = 1; tempo[1] = 7;  personagem.vida = 10; personagem.fome = 15; personagem.segurança = 5;
                 continue
-            } else if (joganuevamiente == 2) {
+            } else if (jogarNovamente == 2) {
                 break
             };
-            // break
-        } // enquanto a vida for menor que 10, repete (se morrer ele breaka e repete)
+        };
 
         if (personagem.segurança < 0){
-            console.log('a segurança da sua casa foi embora e os zumbis invadiram! (< 0)!');
-            console.log(`voce deseja jogar nuevamiente? 1 pra sim e 2 pra nao`);
-            let joganuevamiente = +prompt();
-            if (joganuevamiente == 1){
+            console.log(`Game over! Motivo: a proteção da sua casa foi pro brejo e os zumbis tomaram conta (x.x') Motivo: Segurança (< 0)`);
+            console.log(`Você deseja jogar novamente? 1. Sim 2. Não`);
+            jogarNovamente = +prompt();
+            if (jogarNovamente == 1){
                 tempo[0] = 1; tempo[1] = 7;  personagem.vida = 10; personagem.fome = 15; personagem.segurança = 5;
                 continue
-            } else if (joganuevamiente == 2) {
+            } else if (jogarNovamente == 2) {
                 break
             };
-            // break
-        } // enquanto a vida for menor que 10, repete (se morrer ele breaka e repete)
+        };
 
         if (personagem.fome > 20){
-            console.log('voce morreu de fome! (>20)');
-            console.log(`voce deseja jogar nuevamiente? 1 pra sim e 2 pra nao`);
-            let joganuevamiente = +prompt();
-            if (joganuevamiente == 1){
+            console.log(`Game over! Motivo: Morreu de fome (kkk)! (> 20)`);
+            console.log(`Você deseja jogar novamente? 1. Sim 2. Não`);
+            jogarNovamente = +prompt();
+            if (jogarNovamente == 1){
                 tempo[0] = 1; tempo[1] = 7;  personagem.vida = 10; personagem.fome = 15; personagem.segurança = 5;
                 continue
-            } else if (joganuevamiente == 2) {
+            } else if (jogarNovamente == 2) {
                 break
             };
-            // break
-        } // enquanto a vida for menor que 10, repete (se morrer ele breaka e repete)
+        };
 
-        // while (personagem[1] > 20){ console.log('voce morreu de fome! (>20)');  tempo[0] = 1; tempo[1] = 7;  personagem.vida = 10; personagem.fome = 15; personagem.segurança = 5; break } // enquanto a vida for maior que 20, repete
-        // while (personagem[2] < 0){ console.log('a segurança da sua casa foi embora e os zumbis invadiram! (< 0)');  tempo[0] = 1; tempo[1] = 7;  personagem.vida = 10; personagem.fome = 15; personagem.segurança = 5; break } // enquanto a segurança for menor que 0
-        
-
-        // if (personagem.vida < 10){
-        //     console.log('voce morreu de vida (<10)!')
-        //     console.log('deseja jogar novamente? 1. sim 2. nao')
-        //     playagain = +prompt()
-        //     if (playagain == 1) { tempo[0] = 1 } else { break }
-        // }
 
 
 
@@ -166,21 +128,15 @@ while (tempo[0] == 1){ // enquanto o dia for 1
     if (tempo[1] > 23){
         tempo[0] = 3
         tempo[1] = 0
-    }
+    };
 
     // Variaveis de encerramento
-    // whiles.morreu()
-    // while (personagem[0] < 0){ console.log('voce morreu de vida (< 0)!');  tempo[0] = 1; tempo[1] = 7;  personagem.vida = 10; personagem.fome = 15; personagem.segurança = 5; break } // enquanto a vida for menor que 10, repete (se morrer ele breaka e repete)
-    // while (personagem[1] > 20){ console.log('voce morreu de fome! (>20)');  tempo[0] = 1; tempo[1] = 7;  personagem.vida = 10; personagem.fome = 15; personagem.segurança = 5; break } // enquanto a vida for maior que 20, repete
-    // while (personagem[2] < 0){ console.log('a segurança da sua casa foi embora e os zumbis invadiram! (< 0)');  tempo[0] = 1; tempo[1] = 7;  personagem.vida = 10; personagem.fome = 15; personagem.segurança = 5; break } // enquanto a segurança for menor que 0
-    // }
-
 
 
     while (tempo[0] == 3){
         console.log(`Dia ${tempo[0]} Hora ${tempo[1]}`);
         console.log(`Vida: ${personagem.vida} | Segurança: ${personagem.segurança} | Fome: ${personagem.fome}`);
-        tempo[1] = +prompt('bote uma hora: ')
+        tempo[1] = +prompt('bote uma hora: ');
         personagem.vida = +prompt('bote um valor de vida: ') //alterar pra variaveis que modificam a vida
         personagem.fome = +prompt('bote um valor de fome: ')
         personagem.segurança = +prompt(`bote um valor de segurança (atual: ${personagem[2]}): `)
@@ -188,20 +144,16 @@ while (tempo[0] == 1){ // enquanto o dia for 1
     if (tempo[1] > 23){
         tempo[0] = 4
         tempo[1] = 0
-    }
+    };
 
-    // Variaveis de encerramento
-    whiles.morreu()
-    // while (personagem[0] < 0){ console.log('voce morreu de vida (< 0)!');  tempo[0] = 1; tempo[1] = 7;  personagem.vida = 10; personagem.fome = 15; personagem.segurança = 5; break } // enquanto a vida for menor que 10, repete (se morrer ele breaka e repete)
-    // while (personagem[1] > 20){ console.log('voce morreu de fome! (>20)');  tempo[0] = 1; tempo[1] = 7;  personagem.vida = 10; personagem.fome = 15; personagem.segurança = 5; break } // enquanto a vida for maior que 20, repete
-    // while (personagem[2] < 0){ console.log('a segurança da sua casa foi embora e os zumbis invadiram! (< 0)');  tempo[0] = 1; tempo[1] = 7;  personagem.vida = 10; personagem.fome = 15; personagem.segurança = 5; break } // enquanto a segurança for menor que 0
-
+    // Encerramento do game
+   
     if (tempo[0] == 4){
-        console.log('parabens, voce conseguiu chegar ate o final e foi resgatato pelo helicoptero')
-        console.log('deseja jogar novamente? 1. sim 2. nao')
-        playagain = +prompt()
-        if (playagain == 1) { tempo[0] = 1 } else { break }
-        }
-    }
-    }
-}
+        console.log('Parabéns!!! Você conseguiu chegar até o 4º dia e foi resgatado pelo helicóptero.');
+        console.log('Deseja jogar novamente? 1. Sim 2. Não');
+        playagain = +prompt();
+        if (playagain == 1) { tempo[0] = 1; } else { break; }
+        };
+    };
+    };
+};
