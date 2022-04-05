1702e114691f05cd9f1f96d3f6e60d0e16351257
maxxcleiton~ <maxcleiton@live.com>
2022-04-05 07:38:14 -0300

    Arrow functions e function de história concluída

--- projeto3.3.js
+++ projeto3.3.js
@@ -8,31 +8,55 @@ console.log();
 // Adicionei com sucesso os arrays 
 // arrumar a logica de mortes
 // código arrumado, agora é adicionar a historia
+// aprendi a fazer arrow functions (adianto danado)
+// ==> falto adicionar o adicionar.maisDia() em alguns lugares
+
 
 let alteraTempo = {
-    somarArrayDia: function(array,numero){ 
-let nn = array[0] + numero // array desejada de alteracao
-array.splice(0,1,nn) // 0 indice/1 n. itens excluidos/nn variavel operacao
-return array[0] },
-    somarArrayHora: function(array,numero){ 
-let nn = array[1] + numero // array desejada de alteracao
-array.splice(1,1,nn) // 0 indice/1 n. itens excluidos/nn variavel operacao
-return array[1] },
-    subtrairArrayDia: function(array,numero){ 
-let nn = array[0] - numero // array desejada de alteracao
-array.splice(0,1,nn) // 0 indice/1 n. itens excluidos/nn variavel operacao
-return array[0] },
-    subtrairArrayHora: function(array,numero){ 
-let nn = array[1] - numero // array desejada de alteracao
-array.splice(1,1,nn) // 0 indice/1 n. itens excluidos/nn variavel operacao
-return array[1] } 
-// encerra alteraTempo
+    somarArrayDia: (array, numero) => {
+        let nn = array[0] + numero; // array desejada de alteracao
+        array.splice(0, 1, nn); // 0 indice/1 n. itens excluidos/nn variavel operacao
+        return array[0];
+    },
+    somarArrayHora: (array, numero) => {
+        let nn = array[1] + numero; // array desejada de alteracao
+        array.splice(1, 1, nn); // 0 indice/1 n. itens excluidos/nn variavel operacao
+        return array[1];
+    },
+    subtrairArrayDia: (array, numero) => {
+        let nn = array[0] - numero; // array desejada de alteracao
+        array.splice(0, 1, nn); // 0 indice/1 n. itens excluidos/nn variavel operacao
+        return array[0];
+    },
+    subtrairArrayHora: (array, numero) => {
+        let nn = array[1] - numero; // array desejada de alteracao
+        array.splice(1, 1, nn); // 0 indice/1 n. itens excluidos/nn variavel operacao
+        return array[1];
+    },
 };
 
 let adicionarHora = {
-    maisHora: function(hora){
+    maisHora: (hora) => {
     alteraTempo.somarArrayHora(tempo,hora)
-    }, 
+    },
+    maisDia1: () => {
+        if (tempo[1] > 23){
+            tempo[0] = 2
+            tempo[1] = 0
+            };
+    },
+    maisDia2: () => {
+        if (tempo[1] > 23){
+            tempo[0] = 3
+            tempo[1] = 0
+        };
+    },
+    maisDia3: () => {
+        if (tempo[1] > 23){
+            tempo[0] = 4
+            tempo[1] = 0
+        };
+    },
 };
 
 let personagem = {
@@ -40,44 +64,17 @@ let personagem = {
     fome: 15,
     vida: 10,
     segurança: 5,
-    stats: function(){
+    stats: () => {
         console.log();
         console.log(`------------------------------`);
         console.log(`Dia ${tempo[0]} Hora ${tempo[1]}:00`);
         console.log(`Vida: ${personagem.vida} | Segurança: ${personagem.segurança} | Fome: ${personagem.fome}`);
         console.log();
-        console.log('- O tempo vai andar de acordo com suas escolhas, não deixe o personagem morrer!\n- Você morre se: vida (< 0) | segurança (< 0) | fome (> 20)');
-        console.log();
     },
 };
 
 let historias = {
-    introdução: function(){
-        console.log(`Você acorda numa manhã normal de domingo...
-        \nvocê vai, toma um banho, escova seus dentes...
-        \nvai até a varanda de casa para sentir o sol no rosto e terminar de acordar...
-        \naté que percebe que lá na rua, existem pessoas andando de uma maneira estranha.
-        \nNesse momento você fica em alerta, corre pro celular e decide ligar pra alguém.\n
-        `);
-        
-        personagem.nome = prompt('Escolha o nome do seu personagem: ');
-        
-        console.log(`\n${personagem.nome}, você tem 3 pessoas para ligar: `);
-        
-        console.log(`
-        1. Pai (chefe de gabinete do presidente)
-        2. Mãe (vive com seu pai)
-        3. Irmão (está fazendo um mochilão pela Argentina)
-        `);
-    },
-    sejaBemVindo: function(){
-        console.log(`\n-"Hoje é o primeiro dia de um mundo que agora está sendo tomado por zumbis, o que você irá fazer?"\n`);
-        console.log(`"Você no momento está sozinho, e precisa sobreviver 4 dias até que o helicóptero chegue para seu resgate."
-- Boa sorte!\n`);
-    }
-};
-
-function decisao(decisao){
+    ligação: () => {
         let repetir = 1;
         while (repetir == 1) {
         console.log(`------------------------------`);
@@ -128,7 +125,70 @@ function decisao(decisao){
             // }
         }
     }
+},
+    introdução: () => {
+        console.log(`Você acorda numa manhã normal de domingo...
+        \nvocê vai, toma um banho, escova seus dentes...
+        \nvai até a varanda de casa para sentir o sol no rosto e terminar de acordar...
+        \naté que percebe que lá na rua, existem pessoas andando de uma maneira estranha.
+        \nNesse momento você fica em alerta, corre pro celular e decide ligar pra alguém.\n
+        `);
+        
+        personagem.nome = prompt('Escolha o nome do seu personagem: ');
+        
+        console.log(`\n${personagem.nome}, você tem 3 pessoas para ligar: `);
+        
+        console.log(`
+        1. Pai (chefe de gabinete do presidente)
+        2. Mãe (vive com seu pai)
+        3. Irmão (está fazendo um mochilão pela Argentina)
+        `);
+    },
+    sejaBemVindo: () => {
+        console.log();
+        console.log(`------------------------------`);
+        console.log(`\n-"Hoje é o primeiro dia de um mundo que agora está sendo tomado por zumbis, o que você irá fazer?"\n`);
+        console.log(`"Você no momento está sozinho, e precisa sobreviver 4 dias até que o helicóptero chegue para seu resgate."
+- Boa sorte!`);
+        console.log();
+        console.log('- O tempo vai andar de acordo com suas escolhas, não deixe o personagem morrer!\n- Você morre se: vida (< 0) | segurança (< 0) | fome (> 20)');
+        console.log();
+    },
+    manhã7h: () => {
+        personagem.stats();
+        console.log(`\n1.   Tomar café (com o que tem disponível) (3h)\n  -10 fome\n2.   Assistir Netflix (6h)\n  + 5 fome\n3.   Treino físico (3h) \n  +5 fome\n4.   Ficar na cama se lamentando (12h)\n  +10 fome`);
+        let manhaEscolha1 = +prompt();
+        if (manhaEscolha1 == 1) { // 1.   Tomar café (com o que tem disponível)
+            adicionarHora.maisHora(3);
+            personagem.fome = personagem.fome - 10;
+            console.log(`\nUfa, agora que eu comi, me sinto melhor para fazer outras coisas.`);
+            prompt(`\nVoltar menu principal (qualquer tecla)__`);
+            historias.manhã7h();
+        } else if (manhaEscolha1 == 2) {
+            adicionarHora.maisHora(6);
+            personagem.fome = personagem.fome + 5;
+            console.log(`\nUm Netflix aqui para esquecer um pouco dos problemas.`);
+            prompt(`\nVoltar menu principal (qualquer tecla)__`);
+            historias.manhã7h();
+        } else if (manhaEscolha1 == 3) {
+            adicionarHora.maisHora(3);
+            personagem.fome = personagem.fome + 5;
+            console.log(`\nUfa, um exercício físico alivia o stress do que está acontecendo.`);
+            prompt(`\nVoltar menu principal (qualquer tecla)__`);
+            historias.manhã7h();
+        } else if (manhaEscolha1 == 4) {
+            adicionarHora.maisHora(12);
+            personagem.fome = personagem.fome + 10;
+            console.log(`Já chorei o que tinha que chorar, e agora?`);
+            prompt(`\nVoltar menu principal (qualquer tecla)__`);
+            historias.manhã7h();
+            // if (time.horas > 23){
+            //     i++}
+        }
+    },
 };
+
+
 
 let tempo = [1, 7] // Dia e Hora
 let jogarNovamente;
@@ -138,15 +198,16 @@ let playagain;
 
 historias.introdução();
 
-decisao(decisao);
+historias.ligação();
 
 
 while (tempo[0] == 1){
 
-        personagem.stats();
+        // personagem.stats(); //escolhi colocar na funcao das escolhas
 
         historias.sejaBemVindo();
 
+        historias.manhã7h();
 
         // como fazer perguntas que impactem na hora ::::
         // let escolha = +prompt('avance 1h digitando 1\navance 2h digitando 2')
@@ -166,11 +227,12 @@ while (tempo[0] == 1){
         personagem.fome = +prompt('bote um valor de fome: ');
         personagem.segurança = +prompt(`bote um valor de segurança (atual: ${personagem.segurança}): `);
 
-
-        if (tempo[1] > 23){
-            tempo[0] = 2
-            tempo[1] = 0
-        };
+        // Variáveis de aumentar o dia
+        adicionarHora.maisDia1();
+        // if (tempo[1] > 23){
+        //     tempo[0] = 2
+        //     tempo[1] = 0
+        // };
 
         // Variáveis de encerramento
         if (personagem.vida < 0){
@@ -213,10 +275,8 @@ while (tempo[0] == 1){
 
 
     while (tempo[0] == 2){
-        console.log();
-        console.log(`------------------------------`);
-        console.log(`Dia ${tempo[0]} Hora ${tempo[1]}:00`);
-        console.log(`Vida: ${personagem.vida} | Segurança: ${personagem.segurança} | Fome: ${personagem.fome}`);
+
+        personagem.stats();
 
 
         tempo[1] = +prompt('bote uma hora: ')
@@ -224,10 +284,12 @@ while (tempo[0] == 1){
         personagem.fome= +prompt('bote um valor de fome: ')
         personagem.segurança = +prompt(`bote um valor de segurança (atual: ${personagem.segurança}): `)
 
-    if (tempo[1] > 23){
-        tempo[0] = 3
-        tempo[1] = 0
-    };
+    // Variáveis de aumentar o dia
+    adicionarHora.maisDia2();
+    // if (tempo[1] > 23){
+    //     tempo[0] = 3
+    //     tempo[1] = 0
+    // };
 
     // Variaveis de encerramento
     if (personagem.vida < 0){
@@ -267,21 +329,22 @@ while (tempo[0] == 1){
     };
 
     while (tempo[0] == 3){
-        console.log();
-        console.log(`------------------------------`);
-        console.log(`Dia ${tempo[0]} Hora ${tempo[1]}:00`);
-        console.log(`Vida: ${personagem.vida} | Segurança: ${personagem.segurança} | Fome: ${personagem.fome}`);
+
+        personagem.stats();
 
 
         tempo[1] = +prompt('bote uma hora: ');
         personagem.vida = +prompt('bote um valor de vida: ') //alterar pra variaveis que modificam a vida
         personagem.fome = +prompt('bote um valor de fome: ')
-        personagem.segurança = +prompt(`bote um valor de segurança (atual: ${personagem[2]}): `)
+        personagem.segurança = +prompt(`bote um valor de segurança (atual: ${personagem.segurança}): `)
 
-    if (tempo[1] > 23){
-        tempo[0] = 4
-        tempo[1] = 0
-    };
+
+    // Variáveis de aumentar o dia
+    adicionarHora.maisDia3();
+    // if (tempo[1] > 23){
+    //     tempo[0] = 4
+    //     tempo[1] = 0
+    // };
 
    // Variaveis de encerramento
     if (personagem.vida < 0){
